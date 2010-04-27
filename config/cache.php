30e149f82a5e8f3d73268df75e60127a077d0270
Sam de Freyssinet <sam@def.reyssi.net>
2010-04-27 20:54:54 +0800

    Updated the File driver and configuration settings to explain how to use it properly
    Full docs comming before final release

--- config/cache.php
+++ config/cache.php
@@ -21,6 +21,33 @@ return array
 		),
 		'instant_death'      => TRUE,               // Take server offline immediately on first fail (no retry)
 	),
+	'memcached' => array
+	(
+		'driver'             => 'memcached',
+		'default_expire'     => 3600,         // Default amount of time a key will be cached if none is provided
+		'persistent'         => FALSE,        // Make persisten connections
+		'persistent_id'      => NULL,         // ID given to persistent connection
+		'options'            => array
+		(
+			'compression'          => FALSE,        // Use compression
+			'serializer'           => 'php',        // Data serializer
+			'hash'                 => 'default',    // Hash to use on item keys
+			'prefix_key'           => '',           // Automatic prefix to item keys
+			'distribution'         => 'modula',     // Method of distributing item keys
+			'libketama_compatible' => TRUE,         // Compatibility with libketama-like behavior
+			'buffer_writes'        => FALSE,        // Enables or disables buffered I/O
+			'binary_protocol'      => FALSE,        // Enable the use of the binary protocol
+			'no_block'             => FALSE,        // Enables or disables asynchronous I/O
+			'tcp_nodelay'          => FALSE,        // Enables or disables the no-delay feature for connecting sockets
+			'connect_timeout'      => 1000,         // In non-blocking mode this set the value of the timeout during socket connection, in milliseconds
+			'retry_timeout'        => 0,            // The amount of time, in seconds, to wait until retrying a failed connection attempt
+			'send_timeout'         => 0,            // Socket sending timeout, in microseconds
+			'recv_timeout'         => 0,            // Socket reading timeout, in microseconds
+			'poll_timeout'         => 1000,         // Timeout for connection polling, in milliseconds
+			'cache_lookups'        => FALSE,        // Enables or disables caching of DNS lookups
+			'server_failure_limit' => 0,            // Specifies the failure limit for server connection attempts
+		),
+	),
 	'memcachetag' => array
 	(
 		'driver'             => 'memcachetag',
